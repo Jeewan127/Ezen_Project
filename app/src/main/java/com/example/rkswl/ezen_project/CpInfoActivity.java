6faf2b38a9c8978c63f48de441d3f5260c5fad8d
rkswl <dlawldhks123@gmail.com>
2018-02-22 16:57:19 +0900

    final_project

---
+++ app/src/main/java/com/example/rkswl/ezen_project/CpInfoActivity.java
@@ -0,0 +1,13 @@
+package com.example.rkswl.ezen_project;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class CpInfoActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_cp_info);
+    }
+}
